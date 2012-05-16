82c270c371c541f11e308a9a0ac983f30a393587
Brandon Phillips <baphillips@gmail.com>
2012-05-16 17:28:04 -0400

    Version 1, initial commit

---
+++ OBJECTIVE_C_SINGLETON.h
@@ -0,0 +1,59 @@
+//
+//  OBJECTIVE_C_SINGLETON.h
+//
+//  Created by Brandon Phillips on 5/16/12.
+//  Copyright (c) 2012 Brandon Phillips. All rights reserved.
+//
+
+#ifndef LexDK_OBJECTIVE_C_SINGLETON_h
+#define LexDK_OBJECTIVE_C_SINGLETON_h
+
+/// This macro implements the various methods needed to make a safe singleton.
+/// Sample usage:
+/// OBJECTIVE_C_SINGLETON_(SomeUsefulManager, sharedSomeUsefulManager)
+/// (with no trailing semicolon)
+
+#define OBJECTIVE_C_SINGLETON(_object_name_, _shared_obj_name_)\
+                                                                                        \
+static _object_name_ *z##_shared_obj_name_ = nil;                                       \
+                                                                                        \
++ (_object_name_ *) _shared_obj_name_                                                   \
+{                                                                                       \
+if (z##_shared_obj_name_ == nil) {                                                      \
+z##_shared_obj_name_ = [NSAllocateObject([self class], 0, NULL) init];                  \
+}                                                                                       \
+                                                                                        \
+return z##_shared_obj_name_;                                                            \
+}                                                                                       \
+                                                                                        \
++ (id) allocWithZone:(NSZone *)zone                                                     \
+{                                                                                       \
+return [[self _shared_obj_name_] retain];                                               \
+}                                                                                       \
+                                                                                        \
+- (id) copyWithZone:(NSZone *)zone                                                      \
+{                                                                                       \
+return self;                                                                            \
+}                                                                                       \
+                                                                                        \
+- (id) retain                                                                           \
+{                                                                                       \
+return self;                                                                            \
+}                                                                                       \
+                                                                                        \
+- (NSUInteger) retainCount                                                              \
+{                                                                                       \
+return NSUIntegerMax;  /* so the singleton object cannot be released */                 \
+}                                                                                       \
+                                                                                        \
+- (oneway void) release                                                                 \
+{                                                                                       \
+                                                                                        \
+}                                                                                       \
+                                                                                        \
+- (id) autorelease                                                                      \
+{                                                                                       \
+return self;                                                                            \
+}                                                                                       \
+
+#endif
--- README.md
+++ README.md
@@ -2,3 +2,12 @@ Objective-C-Singleton-Macro
 ===========================
 
 This is a simple singleton enforcement macro.
+
+The singleton implementation is primarily based off the Objective-C Phrasebook (2nd Edition) (Developer's Library) by David Chisnall.
+The macro definition is inspired by the Google toolbox for mac.
+
+Book Link:
+http://www.amazon.com/gp/product/0321813758/ref=as_li_ss_tl?ie=UTF8&tag=brandphill-20&linkCode=as2&camp=1789&creative=390957&creativeASIN=0321813758
+
+Google ToolBox:
+http://code.google.com/p/google-toolbox-for-mac/
