69fa41d05f808be0db83153bddcb064dad6c611c
Brandon Phillips <baphillips@gmail.com>
2012-07-12 14:30:05 -0400

    Update for ARC

--- OBJECTIVE_C_SINGLETON.h
+++ OBJECTIVE_C_SINGLETON.h
@@ -10,50 +10,71 @@
 
 /// This macro implements the various methods needed to make a safe singleton.
 /// Sample usage:
-/// OBJECTIVE_C_SINGLETON(SomeUsefulManager, sharedSomeUsefulManager)
+/// OBJECTIVE_C_SINGLETON_(SomeUsefulManager, sharedSomeUsefulManager)
 /// (with no trailing semicolon)
+#if __has_feature(objc_arc)
 
-#define OBJECTIVE_C_SINGLETON(_object_name_, _shared_obj_name_)\
-                                                                                        \
-static _object_name_ *z##_shared_obj_name_ = nil;                                       \
-                                                                                        \
+#define OBJECTIVE_C_SINGLETON(_object_name_, _shared_obj_name_)                         \
+																						\
 + (_object_name_ *) _shared_obj_name_                                                   \
 {                                                                                       \
-if (z##_shared_obj_name_ == nil) {                                                      \
-z##_shared_obj_name_ = [NSAllocateObject([self class], 0, NULL) init];                  \
+	static _object_name_ *z##_shared_obj_name_;											\
+																						\
+	static dispatch_once_t done;														\
+																						\
+	dispatch_once(&done, ^{z##_shared_obj_name_ = [_object_name_ new]; });				\
+																						\
+	return z##_shared_obj_name_;                                                        \
+}																						\
+
+
+#else
+
+#define OBJECTIVE_C_SINGLETON(_object_name_, _shared_obj_name_)                         \
+																						\
+static _object_name_ *z##_shared_obj_name_ = nil;                                       \
+																						\
++ (_object_name_ *) _shared_obj_name_                                                   \
+{                                                                                       \
+	if (z##_shared_obj_name_ == nil) {                                                  \
+		z##_shared_obj_name_ = [NSAllocateObject([self class], 0, NULL) init];			\
+	}                                                                                   \
+																						\
+	return z##_shared_obj_name_;                                                        \
 }                                                                                       \
-                                                                                        \
-return z##_shared_obj_name_;                                                            \
-}                                                                                       \
-                                                                                        \
+																						\
 + (id) allocWithZone:(NSZone *)zone                                                     \
 {                                                                                       \
-return [[self _shared_obj_name_] retain];                                               \
+	return [[self _shared_obj_name_] retain];											\
 }                                                                                       \
-                                                                                        \
+																						\
 - (id) copyWithZone:(NSZone *)zone                                                      \
 {                                                                                       \
-return self;                                                                            \
+return self;																			\
 }                                                                                       \
-                                                                                        \
+																						\
 - (id) retain                                                                           \
 {                                                                                       \
-return self;                                                                            \
+	return self;                                                                        \
 }                                                                                       \
-                                                                                        \
+																						\
 - (NSUInteger) retainCount                                                              \
 {                                                                                       \
-return NSUIntegerMax;  /* so the singleton object cannot be released */                 \
+	return NSUIntegerMax;  /* so the singleton object cannot be released */             \
 }                                                                                       \
-                                                                                        \
+																						\
 - (oneway void) release                                                                 \
 {                                                                                       \
-                                                                                        \
+																						\
 }                                                                                       \
-                                                                                        \
+																						\
 - (id) autorelease                                                                      \
 {                                                                                       \
-return self;                                                                            \
+	return self;                                                                        \
 }                                                                                       \
+
+
+#endif
+
 
 #endif
